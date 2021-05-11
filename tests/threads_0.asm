2d98b7efd0ef9f9286381604fc78ef3d8674ea88
Cristian-A <cristianantonuccio@icloud.com>
2021-05-11 23:41:32 +0200

    fix

--- tests/threads_0.asm
+++ tests/threads_0.asm
@@ -7,17 +7,36 @@
 
 main:
 
-	push.d print
-	call.k fork
-	push.w 5000
-	call.k sleep
+	push.d    thread_1         ; push thread address
+	call.k    fork             ; call fork
+	push.w    5000             ; push 5 seconds
+	call.k    sleep            ; call sleep
 
-	halt
+	halt                       ; end program (every thread)
 
-print:
+; ======== Thread 1 ========================================== ;
 
-	call.k sign
-	push.w 300
-	call.k sleep
+thread_1:
 
-	jump print
+	push.b    string_0_length  ; push the string length
+	call.k    allocate         ; allocate n bytes
+	push.b    string_0_length  ; push the string length
+	get.g     0                ; get allocated address
+	push.d    string_0         ; get code address
+	call.k    load             ; load code string in memory
+
+loop:
+
+	top                        ; duplicate string address
+	call.k    ostream          ; print string (pops)
+	push.w    300              ; push 300ms (.3s)
+	call.k    sleep            ; call sleep
+	jump      loop             ; loop forever
+
+	halt                       ; safety halt (just in case)
+
+; ======== DATA SECTION ======================================= ;
+
+string_0:
+	#d "doing something\n", 0x00
+	string_0_length = $ - string_0
