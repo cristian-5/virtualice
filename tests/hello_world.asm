19f739bc36eb85368d81419e6f74ab1fff1da529
cristian-5 <cristianantonuccio@icloud.com>
2023-09-03 12:43:21 +0200

    better hello world

--- tests/hello_world.asm
+++ tests/hello_world.asm
@@ -1,24 +1,10 @@
 
 #include "ice.asm"
 
-main: STRPTR = 0
+main:
 
-	; allocate enough memory:
-	push.b     string_0_length
-	call.k     allocate
-	global.b.c STRPTR            ; store allocation ptr
-
-	; load the string in memory:
-	push.b     string_0_length   ; number of bytes to copy
-	global.b.g STRPTR            ; get allocation ptr (destination)
-	push.d     string_0          ; code pointer (source)
-	call.k     load              ; fun load(s: u32, d: *, n: u64): *;
-
-	; print the string and cleanup:
-	global.b.g STRPTR            ; get allocation ptr
-	top                          ; duplicate string pointer
-	call.k    ostream
-	call.k    deallocate         ; deallocate leftover pointer
+	push.a    string_0           ; push string_0 onto the stack
+	call.k    odata              ; print string from data segment
 
 	halt
 
